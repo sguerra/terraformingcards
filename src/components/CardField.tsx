0244b034137d21959d5d0e2388c8245f64617bab
Adrián Ortega <adrianog.sw@gmail.com>
2023-03-05 15:05:28 -0600

    chore: Fix placement of label and input boxes

--- src/components/CardField.tsx
+++ src/components/CardField.tsx
@@ -10,8 +10,8 @@ export const CardField: FunctionComponent<CardFieldProps> = ({ label, inputType 
   const hasInputType = Boolean(inputType)
   return (
     <div className={`flex px-7 py-1 ${hasInputType ? 'justify-between' : 'justify-center items-center'} w-full`}>
-      <label className={`${hasInputType ? 'flex-shrink' : 'flex-grow text-center'} pr-2`}>{label}</label>
-      {hasInputType && (<input className='flex-grow rounded-md bg-black bg-opacity-50 border-white border-2' type={inputType} />)}
+      <label className={`${hasInputType ? 'flex-grow text-right pr-3' : 'flex-grow text-center'} pr-2`}>{label}</label>
+      {hasInputType && (<input className='w-45 justify-end rounded-md bg-black bg-opacity-50 border-white border-2' type={inputType} />)}
     </div>
   )
 }
