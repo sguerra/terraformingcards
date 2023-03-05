4059e8f30bfc2400611493494b3aada82afd46a8
Adrián Ortega <adrianog.sw@gmail.com>
2023-03-05 15:51:35 -0600

    chore: Added select field and include range in number fields

--- src/components/CardEditor.tsx
+++ src/components/CardEditor.tsx
@@ -9,11 +9,11 @@ export const CardEditor: FunctionComponent<CardEditorProps> = () => {
   return (
     <div className='bg-contain pt-12 h-[478px] w-[334px] bg-[url("img/tmtemplates/resources/card.png")]'>
       <CardField label='Nombre:' inputType='text' />
-      <CardField label='Megacréditos:' inputType='number' />
-      <CardField label='Tags:' inputType='text' />
+      <CardField label='Megacréditos:' inputType='number' numberRange={{ min: 1, max: 99 }} />
+      <CardField label='Tags:' inputType='select' />
       <CardField label='Imagen:' inputType='text' />
       <CardField label='Requisitos:' inputType='text' />
-      <CardField label='Puntos de victoria:' inputType='number' />
+      <CardField label='P de victoria:' inputType='number' numberRange={{ min: -10, max: 15 }} />
       <CardField label='Expansión:' inputType='text' />
       <CardField label='Efectos' />
       <CardField label='Íconos:' inputType='text' />
--- src/components/CardField.tsx
+++ src/components/CardField.tsx
@@ -3,15 +3,30 @@ import { FunctionComponent, ReactNode } from 'react'
 interface CardFieldProps {
   children?: ReactNode
   label: string
-  inputType?: 'text' | 'number'
+  inputType?: 'text' | 'number' | 'select'
+  numberRange?: { min: number, max: number }
 }
 
-export const CardField: FunctionComponent<CardFieldProps> = ({ label, inputType }) => {
+export const CardField: FunctionComponent<CardFieldProps> = ({ label, inputType, numberRange = { min: 0, max: 0 } }) => {
   const hasInputType = Boolean(inputType)
   return (
     <div className={`flex px-7 py-1 ${hasInputType ? 'justify-between' : 'justify-center items-center'} w-full`}>
-      <label className={`${hasInputType ? 'flex-grow text-right pr-3' : 'flex-grow text-center'} pr-2`}>{label}</label>
-      {hasInputType && (<input className='w-45 justify-end rounded-md bg-black bg-opacity-50 border-white border-2' type={inputType} />)}
+      <label className={`${hasInputType ? 'w-28 truncate !text-right pr-2' : 'flex-grow text-center'} pr-2`}>{label}</label>
+
+      {hasInputType && inputType === 'text' && (
+        <input className='flex-grow  rounded-md bg-black bg-opacity-50 border-white border-2' type={inputType} />)}
+
+      {hasInputType && inputType === 'number' && (
+        <input
+          className='flex-grow text-right rounded-md bg-black bg-opacity-50 border-white border-2' type={inputType}
+          min={numberRange.min} max={numberRange.max}
+        />)}
+
+      {hasInputType && inputType === 'select' && (
+        <select className='flex-grow rounded-md bg-black bg-opacity-50 border-white border-2'>
+          <option value='None'>None</option>
+          <option value='Plant'>Plant</option>
+        </select>)}
     </div>
   )
 }
