299779ae51d6251f766f4a6d0a652795cc782ba9
Sergio Guerra <sergioguerra1991@gmail.com>
2023-03-05 17:01:18 -0600

    fix: CardPreview bg images to img

--- src/components/CardViewer.tsx
+++ src/components/CardViewer.tsx
@@ -17,9 +17,9 @@ interface CardViewerProps {
 
 export const CardViewer: FunctionComponent<CardViewerProps> = ({
   name = 'Sample Card',
-  mc = 5,
-  tag = Tags.Plant,
-  vps = 1
+  mc = 99,
+  tag = Tags.Earth,
+  vps = 5
 }) => {
   return (
     <div className='h-full flex flex-grow items-center justify-end mr-5'>
@@ -28,11 +28,11 @@ export const CardViewer: FunctionComponent<CardViewerProps> = ({
           <img src='img/upload-icon.png' alt='Upload' className='opacity-50 h-16' />
         </div>
         <div className='relative w-[334px] h-[478px] bg-[url("img/tmtemplates/templates/green_normal.png")] bg-contain bg-no-repeat bg-center'>
-          <span className='absolute text-black left-7 top-7 text-2xl'>{mc}</span>
+          <span className='absolute flex justify-center text-black left-3 top-7 text-2xl w-11'>{mc}</span>
           <div className='absolute flex justify-center items-center left-16 top-7 w-20 h-8 bg-[url("img/tmtemplates/requisites/min_small.png")] bg-contain bg-no-repeat'>
             <div className='w-6 h-6 bg-[url("img/tmtemplates/tiles/greenery-no-o2.png")] bg-contain bg-no-repeat' />
           </div>
-          <div className={`absolute flex justify-center items-center right-5 top-5 w-12 h-12 bg-[url("img/tmtemplates/tags/${tag}.png")] bg-contain bg-no-repeat`} />
+          <img className='absolute right-5 top-5 w-12 h-12' src={`img/tmtemplates/tags/${tag}.png`} alt='tag' />
           <div className='flex text-black absolute w-full justify-center top-16 text-xl uppercase'>{name}</div>
           <div className='text-white absolute left-32 top-[246px] blur-sm'>
             FANMADE
@@ -40,7 +40,7 @@ export const CardViewer: FunctionComponent<CardViewerProps> = ({
           <div className='text-black absolute left-32 top-[246px]'>
             FANMADE
           </div>
-          <div className={`absolute flex justify-center items-center right-5 bottom-14 w-24 h-24 bg-[url("img/tmtemplates/VPs/${vps}.png")] bg-contain bg-no-repeat`} />
+          <img className='absolute right-5 bottom-14 w-24 flex justify-center' src={`img/tmtemplates/VPs/${vps}.png`} alt='VPs' />
         </div>
       </div>
     </div>
